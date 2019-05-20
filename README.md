5822c52cc05f87ad8b06186593079596e35976ff
Stan Barkmeijer <stanbarkmeijer@hotmail.com>
2019-05-20 23:54:19 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Aeternum
+# Aeternum ⏳
+This open-source repository holds my new Discord Bot named Aeternum.
+
+## Questions ⁉
+If you have any questions, contact me on Discord: @Stan#9999, or on GitHub itself.
+
---
+++ main.js
@@ -0,0 +1,29 @@
+const { Client } = require("discord.js");
+const { token } = require("./token.json");
+
+const bot = new Client();
+
+bot.on("ready", async () => {
+    const url = await bot.generateInvite("ADMINISTRATOR");
+    
+    bot.user.setPresence({
+        game: {
+            name: `on ${bot.guilds.size} serve${bot.guilds.size === 1 ? 'r' : 'rs'} 💻`
+        },
+        status: "online"        
+    });
+
+    console.log(`\n\n --+ ${bot.user.username} is now online +--\n --+ ${url} +-- \n\n`);
+});
+
+bot.on("message", async message => {
+    if (message.author.bot || !message.guild) return;
+
+    // If the message.member is uncached, message.member might return null. 
+    // This prevents that from happening. 
+    !message.member ? message.member = await message.guild.fetchMember(message.author) : message.member;
+
+    console.log(`📧\t ${message.author.tag} : ${message.content}`);
+});
+
+bot.login(token);
